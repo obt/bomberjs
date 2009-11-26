c35bd1dbd8bd3c8da46d5c9158f9b068021bcf1e
Benjamin Thomas <benjamin@benjaminthomas.org>
2009-11-26 03:50:58 -0700

    Fixed problem with example view and made it so we actually pass the paramaters to our psuedo-request

--- exampleProject/routes.js
+++ exampleProject/routes.js
@@ -7,13 +7,10 @@ r.add('/', { view: 'simple', action: 'index' });
 // will return a 500 error as the view doesn't exist
 r.add('/section', { view: 'simple', action: 'section' }) 
 
-r.add('/section/:id.:format', { view: 'simple', action: 'show' })
-r.add('/section/:id', { view: 'simple', action: 'show', format: 'html' })
+r.add('/section/:id', { view: 'simple', action: 'show', id: '[0-9]+' })
 
 // default catch all routes
-r.add('/:view/:action/:id.:format');
 r.add('/:view/:action/:id');
-r.add('/:view/:action.:format');
 r.add('/:view/:action');
 
 exports.router = r;
--- exampleProject/views/simple.js
+++ exampleProject/views/simple.js
@@ -1,8 +1,10 @@
+var sys = require('sys');
+
 exports.index = function(request, response) {
   return "index action";
 };
 exports.show = function(request, response) {
-  if( request.parameters.format == 'json' ) {
+  if( request.params.format == 'json' ) {
     return {a: 1, b: 'two', c: { name: 'three'}};
   }
   else {
