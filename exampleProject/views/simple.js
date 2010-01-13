796d9ff0c58f1e90f0b2842a5750e46f39b350a8
Benjamin Thomas <benjamin@benjaminthomas.org>
2010-01-12 17:14:50 -0700

    Switched to new action implementation that depends on promises.
    Now, an action is just ran, and if it wants to do async stuff, it
    needs to return a promise.  Much simpler.  Also added support for
    breaking out of an action early (if you are using promises, by
    throwing a certain kind of object).
    
    Still need tests.

--- exampleProject/views/simple.js
+++ exampleProject/views/simple.js
@@ -1,11 +1,13 @@
 var sys = require('sys');
 
+var HTTP301MovedPermanently = require('bomberjs/lib/http_responses').HTTP301MovedPermanently;
+
 exports.index = function(request, response) {
   return "index action";
 };
 exports.show = function(request, response) {
-  if( request.params.format == 'json' ) {
-    return {a: 1, b: 'two', c: { value: 'three'}};
+  if( request.params.id == 2 ) {
+    return new HTTP301MovedPermanently('http://google.com');
   }
   else {
     return "show action";
