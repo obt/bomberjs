b2a233153bb887417a038bbeee4b844931f28286
Benjamin Thomas <benjamin@benjaminthomas.org>
2010-01-13 01:07:02 -0700

    Fixed bug in router and added more tests

--- test/test-router.js
+++ test/test-router.js
@@ -5,8 +5,10 @@ var Router = require('../lib/router').Router;
 
 // Simple routes
 var r = new Router();
+r.add('/(a)(b)(c)', { view: 'view_name', action: 'action_name' });
 r.add('/defer', "subApp");
 r.add('/:action/:id/:more', { view: 'view_name' });
+r.add('/:action/:id', { view: 'view_name_int', id: '[0-9]+' });
 r.add('/:action/:id', { view: 'view_name' });
 r.add('/:action', { view: 'view_name' });
 r.add('/', { view: 'view_name', action: 'action_name' });
@@ -17,6 +19,15 @@ var tests = [
         action: 'action_name'
       },
       params: {
+      }
+    } ],
+  [ ['GET', '/abc'],
+    { action: {
+        view: 'view_name',
+        action: 'action_name'
+      },
+      params: {
+        args: ['a','b','c']
       }
     } ],
   [ ['GET', '/action_name'],
@@ -39,20 +50,29 @@ var tests = [
     } ],
   [ ['GET', '/action_name/1'],
     { action: {
-        view: 'view_name',
+        view: 'view_name_int',
         action: 'action_name'
       },
       params: {
         id: "1"
       }
     } ],
-  [ ['GET', '/action_name/1/'],
+  [ ['GET', '/action_name/string'],
     { action: {
         view: 'view_name',
         action: 'action_name'
       },
       params: {
-        id: "1"
+        id: "string"
+      }
+    } ],
+  [ ['GET', '/action_name/string/'],
+    { action: {
+        view: 'view_name',
+        action: 'action_name'
+      },
+      params: {
+        id: "string"
       }
     } ],
   [ ['GET', '/defer/more/path'],
