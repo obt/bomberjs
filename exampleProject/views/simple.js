593ce8913245f112af4f2ba25c1425d9ed3acfc4
Benjamin Thomas <benjamin@benjaminthomas.org>
2010-01-14 03:45:05 -0800

    Added an action to the exampleProject that uses a Promise

--- exampleProject/views/simple.js
+++ exampleProject/views/simple.js
@@ -1,4 +1,6 @@
 var sys = require('sys');
+var posix = require('posix');
+var path = require('path');
 
 var HTTP301MovedPermanently = require('bomberjs/lib/http_responses').HTTP301MovedPermanently;
 
@@ -12,4 +14,15 @@ exports.show = function(request, response) {
   else {
     return "show action";
   }
+};
+
+
+// can be accessed at /simple/lorem
+exports.lorem = function(request, response) {
+  // posix.cat returns a Node promise. Which at this time isn't chainable
+  // so this example is pretty simple.  But once we can chain, I'll show
+  // how to manipulate the result as you move through the chain.
+  
+  var filename = path.join(path.dirname(__filename),'../resources/lorem-ipsum.txt');
+  return posix.cat(filename);
 };
