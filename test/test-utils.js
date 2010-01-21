4f4067a740d56a743037692b73b66371536fe90d
Benjamin Thomas <benjamin@benjaminthomas.org>
2010-01-20 23:53:47 -0700

    Added more mimetypes and added charsets lookup for mimetypes
    Also added some tests and removed redundant lookupExtension function.

--- test/test-utils.js
+++ test/test-utils.js
@@ -28,11 +28,24 @@ var tests = {
     // easy
     assert.equal('text/plain', utils.mime.lookup('text.txt'));
     
+    // just an extension
+    assert.equal('text/plain', utils.mime.lookup('.txt'));
+    
     // default
     assert.equal('application/octet-stream', utils.mime.lookup('text.nope'));
 
     // fallback
     assert.equal('fallback', utils.mime.lookup('text.fallback', 'fallback'));
+  },
+  "test charset lookup": function() {
+    // easy
+    assert.equal('UTF-8', utils.charsets.lookup('text/plain'));
+    
+    // none
+    assert.ok(typeof utils.charsets.lookup('text/nope') == 'undefined');
+
+    // fallback
+    assert.equal('fallback', utils.charsets.lookup('text/fallback', 'fallback'));
   }
 };
 
