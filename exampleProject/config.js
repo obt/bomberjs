74212ad89d0689e9732357d8fd2d9ff4aee3d8c4
Steffen Tiedemann Christensen <steffen@refresh.dk>
2010-02-07 14:23:26 +0100

    JavaScriptified the notation in config files (see http://groups.google.com/group/bomberjs/msg/552d14ef3e574fa4 for details)
    This commit also provides a mechanism for default configuration to be
    merged with project/app configs

--- exampleProject/config.js
+++ exampleProject/config.js
@@ -1,18 +1,60 @@
-/* PROJECT CONFIGURATION FILE */
 
-// Port to run node server on (default 8400)
-// exports.port = 8400;
+/* SERVER */
+exports.server = {
+  // Port to run node server on 
+  // Default: 8400
+  port: 8400
+}
 
-// A secret key to sign sessions, cookies, password etc. 
-// Make sure you set this, and that you keep the secret -- well, secret
-exports.signing_secret = "";
+/* SECURITY */
+exports.security = {
+  // A secret key to sign sessions, cookies, password etc. 
+  // Make sure you set this, and that you keep the secret -- well, secret
+  signing_secret: ""
+}
 
-// Configuration for persistent storage for session and users
-// (at the moment, only on-disk storage is available)
-// Default: 'disk'
-exports.persistent_storage_method = 'disk';
 
-// For disk storage, this is the path a folder
-// Default: '/tmp/'
-exports.persistent_storage_location = './storage/';
+/* PERSISTENT STORAGE */
+exports.persistent_storage = {
+  // Configuration for persistent storage for session and users
+  // Options are 'disk' and 'couchdb'
+  // Default: 'disk'
+  method: 'disk',
+
+  // Options for the persistent storage method
+  // Default: {location:'/tmp/'}
+  options: {
+    location: './storage/'
+   }
+}
+
+
+// SESSIONS
+exports.session = {
+  // Minutes before sessions expire
+  // Default: 600 (10 hours)
+  expire_minutes: 600,
+
+  // Minutes before a session is renewed
+  // Default: 10 (10 minutes)
+  renew_minutes: 10,
+
+  cookie: {
+    // Name of the cookie to use for session
+    // Default: 'session_key'
+    name: 'session_key',
+    
+    // Domain for the session cookie
+    // Default: ''
+    domain: '',
+    
+    // Path for the session cookie
+    // Default: '/'
+    path: '/',
+    
+    // Whether to require session cookies only to be sent over secure connections (Boolean)
+    // Default: false
+    secure: false
+  }
+}
 
