b2183a617d632471a83bbc35c8deccdf3ab2d4d2
Benjamin Thomas <benjamin@benjaminthomas.org>
2010-02-07 23:44:40 -0700

    Some additions to the example project.  Basically, using cookies and sesssions

--- exampleProject/routes.js
+++ exampleProject/routes.js
@@ -3,6 +3,7 @@ var Router = require('bomberjs/lib/router').Router;
 var r = new Router();
 
 r.add('/', { view: 'simple', action: 'index' });
+r.add('/env', { view: 'simple', action: 'env' });
 
 // will return a 500 error as the view doesn't exist
 r.add('/section', { view: 'simple', action: 'section' }) 
