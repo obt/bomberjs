ef94357ecb7ed0d067079023075cd7f3a68b39c6
Benjamin Thomas <benjamin@benjaminthomas.org>
2010-02-14 00:24:01 -0800

    Changed the way the 'chainable' library worked. Trying to get a consistent API for listening to server and request events.  Updated the server to match the new API

--- test/test-chainable_events.js
+++ test/test-chainable_events.js
@@ -2,7 +2,7 @@ var sys = require('sys');
 
 var TestSuite = require('../dependencies/node-async-testing/async_testing').TestSuite;
 
-var makeChainable = require('../lib/chainable_events').makeChainable;
+var makeChainable = require('../lib/chainable').makeChainable;
 
 (new TestSuite('Action Tests'))
   .setup(function() {
@@ -12,7 +12,7 @@ var makeChainable = require('../lib/chainable_events').makeChainable;
       this.obj = new Func();
     })
   .runTests({
-    "test add listener": function() {
+    "test add listeners": function() {
       // listeners object should not exist if no listeners have been added
       this.assert.ok(!this.obj._listeners);
 
@@ -31,82 +31,75 @@ var makeChainable = require('../lib/chainable_events').makeChainable;
       this.assert.equal(1, this.obj._listeners.event2.length);
     },
     "test emit with no listeners": function(test) {
+      test.numAssertionsExpected = 1;
+
+      var finished = function(val) {
+        test.assert.equal(1, val);
+        test.finish();
+      };
+
+      test.obj.emit('event', finished, null, 1);
+    },
+    "test emit with listeners": function(test) {
       test.numAssertionsExpected = 3;
 
-      var finished = function(one, two, three) {
-        test.assert.equal(1, one);
-        test.assert.equal(2, two);
-        test.assert.equal(3, three);
+      var finished = function(val) {
+        test.assert.equal(3, val);
         test.finish();
       };
 
-      test.obj.emit('event', finished, null, 1, 2, 3);
-    },
-    "test emit with listener": function(test) {
-      test.numAssertionsExpected = 6;
-
-      var finished = function(one, two, three) {
-        test.assert.equal(1, one);
-        test.assert.equal(2, two);
-        test.assert.equal(3, three);
-        test.finish();
-      };
-
-      test.obj.listen('event', function(cb, one, two, three) {
-          test.assert.equal(1, one);
-          test.assert.equal(2, two);
-          test.assert.equal(3, three);
-
-          cb();
+      test.obj.listen('event', function(obj) {
+          test.assert.equal(1, obj);
+          return 2
+        });
+      test.obj.listen('event', function(obj) {
+          test.assert.equal(2, obj);
+          return 3
         });
 
-      test.obj.emit('event', finished, null, 1, 2, 3);
-    },
-    "test listener can cancel event": function(test) {
-      test.numAssertionsExpected = 1;
-      var canceled = finished = function(val) {
-        test.assert.equal('val', val);
-      };
-
-      // this listener cancels the event
-      test.obj.listen('event', function(cb) {
-          cb('val');
-        });
-
-      // this listener will never get called
-      test.obj.listen('event', function(cb) {
-          test.assert.ok(false);
-          cb();
-        });
-
-      test.obj.emit('event', finished, canceled);
-      test.obj.emit('finished', function() { test.finish(); });
+      test.obj.emit('event', finished, null, 1);
     },
     "test this always points to the emitting object": function(test) {
-      test.numAssertionsExpected = 5;
+      test.numAssertionsExpected = 3;
 
-      var canceled = finished = function() {
+      var finished = function() {
         test.assert.equal(test.obj, this);
+        test.finish();
       };
 
-      // these listeners return normally
-      test.obj.listen('event1', function(cb) {
+      test.obj.listen('event', function() {
           test.assert.equal(test.obj, this);
-          cb();
         });
-      test.obj.listen('event1', function(cb) {
+      test.obj.listen('event', function() {
           test.assert.equal(test.obj, this);
-          cb();
         });
 
-      // this listener cancels the event
-      test.obj.listen('event2', function(cb) {
-          test.assert.equal(test.obj, this);
-          cb(true);
+      test.obj.emit('event', finished);
+    },
+    "test step function gets called after every listener": function(test) {
+      test.numAssertionsExpected = 5;
+
+      var finished = function() {
+        test.assert.equal(test.obj, this);
+        test.finish();
+      };
+
+      var count = 0;
+
+      var step = function(val) {
+        count++;
+        test.assert.equal(val, count);
+      };
+
+      test.obj.listen('event', function(val) {
+          test.assert.equal(0, val);
+          return val+1;
+        });
+      test.obj.listen('event', function(val) {
+          test.assert.equal(1, val);
+          return val+1;
         });
 
-      test.obj.emit('event1', finished);
-      test.obj.emit('event2', finished, canceled);
-      test.obj.emit('event3', function() { test.finish(); });
+      test.obj.emit('event', finished, step, 0);
     },
   });
