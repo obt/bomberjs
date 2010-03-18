43b8901033089722f76babd74e012f1597866352
Benjamin Thomas <benjamin@benjaminthomas.org>
2010-03-17 18:40:39 -0600

    Clean up simple example's routes file

--- examples/simple/routes.js
+++ examples/simple/routes.js
@@ -1,20 +1,16 @@
 var Router = require('bomberjs/apps/http/lib/router').Router;
 
-var r = new Router();
+var r = exports.router = new Router();
 
+// serve files in ./resources
+r.addFolder();
+
+// specific routes
 r.add('/', { view: 'simple', action: 'index' });
 r.add('/env', { view: 'simple', action: 'env' });
-
-// will return a 500 error as the view doesn't exist
-r.add('/section', { view: 'simple', action: 'section' }) 
-
 r.add('/section/:id', { view: 'simple', action: 'show', id: '[0-9]+' })
-
-r.addFolder();
-r.addFolder({path:'/photos/'});
 
 // default catch all routes
 r.add('/:view/:action/:id');
 r.add('/:view/:action');
 
-exports.router = r;
