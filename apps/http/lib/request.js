ef478f9c6accffeb6bfefc194f814188da4e7fd7
Benjamin Thomas <benjamin@benjaminthomas.org>
2010-03-17 19:07:38 -0600

    Remove Request.prototype.loadData because it was broken

--- apps/http/lib/request.js
+++ apps/http/lib/request.js
@@ -1,26 +1,7 @@
 var querystring = require('querystring');
-
 var promise = require('bomberjs/bundled/promise');
-
 var makeChainable = require('bomberjs/lib/chainable').makeChainable;
 
-/* Request(req, url, route)
- *
- * A Bomber Request is a wrapper around the node.js `http.ServerRequest` object.
- *
- * It will basically add some niceties like, waiting for and parsing of POST
- * data and  easy manipulation of cookies and session variables. But currently
- * it does nothing.
- *
- * Parameters:
- *
- * + `req`: An `http.ServerRequest` instance.
- * + `url`: `Object`: A parsed URL for this request. The server parses the url
- *   so it can send it to the app to determine the action.  We don't want to
- *   parse it again, so we pass it here.
- * + `route`: `Object`.  The route object returned from the router.  See
- *   `Router.prototype.findRoute` for details.
- */
 var Request = exports.Request = function(req, url, route) {
   this._request = req;
 
@@ -30,13 +11,10 @@ var Request = exports.Request = function(req, url, route) {
   this.headers = req.headers;
 
   if( route ) {
-    this._action = route.action;
-    this.url = url;
-    this.params = process.mixin({}, route.params, this.url.query);
+    this.setRoute(url, route);
   }
   else {
-    this.url = null;
-    this.params = null;
+    this.url = url;
   }
   
   this.data = null;
@@ -51,70 +29,8 @@ var Request = exports.Request = function(req, url, route) {
 };
 makeChainable(Request);
 
-Request.prototype.addDetails = function(url, route) {
+Request.prototype.setRoute = function(url, route) {
   this._action = route.action;
   this.url = url;
   this.params = process.mixin({}, route.params, this.url.query);
-};
-
-/* Request.prototype.loadData(parseData)
- *
- * Returns a promise that is called with the body of this request. 
- *
- * Node doesn't wait for the entire body of a request to be received  before
- * starting the callback for a given request.  Instead we have to listen for
- * 'body' and 'complete' events on the request to receive and know when the
- * entire request has been loaded. That's what this function does.
- *
- * Additionally `Request.prototype.loadData` will parse the body of the 
- * request using `querystring.parse()`. This can be turned off with `parseData`
- * parameter.
- *
- * Parameters:
- *
- * + `parseData`: `Boolean`.  Whether or not to parse the loaded data with
- *   `querystring.parse()`.  Default is true.
- *
- * Returns:
- *
- * A Promise that will be fullfilled with the loaded (and maybe parsed data)
- * for this request.
- */
-Request.prototype.loadData = function(parseData) {
-  if( typeof parseData == 'undefined' ) {
-    parseData = true;
-  }
-
-  var p = new promise.Promise();
-  var self = this;
-
-  if( this.data === null ) {
-    var data = '';
-
-    this.listen('body', function(str) {
-        data += str;
-      });
-
-    this.listen('finished', function(str) {
-        if( str ) {
-          data += str;
-        }
-
-        if( parseData ) {
-          self.data = querystring.parse(data);
-        }
-        else {
-          self.data = data;
-        }
-
-        p.emitSuccess(self.data);
-      });
-  }
-  else {
-    process.nextTick(function() {
-        p.emitSuccess(self.data);
-      });
-  }
-
-  return p;
 };
