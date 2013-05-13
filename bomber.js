a248fc67ff1e97663cc957459e1de9620ef2bbc4
Benjamin Thomas <benjamin@benjaminthomas.org>
2013-05-13 15:26:48 -0700

    simplification rewrite

--- bomber.js
+++ bomber.js
@@ -1,64 +1,36 @@
 #! /usr/bin/env node
 
-/* bomber.js
- *
- * bomber.js is a shell script that is used to manage your Bomber projects.
- *
- * Syntax:
- * -------
- *
- *     bomber.js <flags> <task>
- *
- * Flags:
- *
- * + `--app <filename>`, `-a <filename>`: Optional.  the name or the path of a
- *   Bomber app. This argument is used by the Node require command, so read up
- *   on how that works to make sure Bomber will be able to find your app. If the
- *   argument isn’t supplied it uses the current directory.
- *
- * Tasks:
- *
- * It loads its commands in from `./lib/tasks`.
- *
- * + `start-server`: Start a bomber server
- * + `run-tests`: Run all javascript files in the `test` folder that start with 
- *   `test-`
- *
- * Examples:
- * ---------
- *
- *     ./bomber.js start-server
- *
- *     ./bomber.js --app ./exampleProject start-server
- */
-
-var sys = require('sys');
-var path = require('path');
-var posix = require('posix');
+var sys = require('sys')
+  , path = require('path')
+  , fs = require('fs')
+  ;
  
-// We depend on bomberjs being on the path, so wrap this require
-// for a module we need in a try catch, and so if bomberjs isn't in
-// the path we can add it.
+// We depend on bomberjs being on the path, so wrap this require for a module we
+// need in a try catch, and if bomberjs isn't in the path add it.
 try {
-  var App = require('bomberjs/lib/app').App;
+  var app = require('bomberjs/lib/app');
 }
 catch(err) {
-  // if that isn't on the path then assume the script being run is
-  // in the source, so add the directory of the script to the path.
   if( err.message == "Cannot find module 'bomberjs/lib/app'" ) {
-    require.paths.push(path.dirname(__filename)+'/..');
-    var App = require('bomberjs/lib/app').App;
+    // if that isn't on the path then assume the script being run is
+    // in the source, so add the directory of the script to the path.
+    require.paths.push(path.join(__dirname,'..'));
+    var app = require('bomberjs/lib/app');
   }
 }
 
+/******** load in default tasks *****/
 // load in all the files from bomberjs/lib/tasks.  These are our
 // base tasks that every app has.  like start_server (and
 // run_tests eventually).  Down the line I'd like apps to be able
 // to write their own tasks
-var bomberjs_location = path.normalize(path.join(require('bomberjs/lib/utils').require_resolve('bomberjs/lib/app'),'../../'));
-var dir = posix.readdir(path.join(bomberjs_location,'lib/tasks')).wait();
-var tasks = {};
-dir.forEach(function(file) {
+var module = require('bomberjs/lib/utils/module')
+  , bomberjsLocation = path.normalize(path.join(module.resolveLocation('bomberjs/lib/app', true),'../../'))
+  , dirContents = fs.readdirSync(path.join(bomberjsLocation,'lib/tasks'))
+  , tasks = {}
+  ;
+
+dirContents.forEach(function(file) {
     if( !file.match(/\.js$/) ) {
       return;
     }
@@ -67,59 +39,51 @@ dir.forEach(function(file) {
   });
 
 
-// ARGV[0] always equals node
-// ARGV[1] always equals bomber.js
-// so we ignore those
+/******** parse arguments *****/
+// ARGV[0] is always node and ARGV[1] is always bomber.js so ignore those
 var argv = process.ARGV.slice(2);
 
-// parse arguments
 var opts = {};
 while( argv.length > 0 ) {
   var stop = false;
   switch(argv[0]) {
-    case "--tasks":
-    case "-t":
-      opts['tasks'] = true;
-      break;
     case "--app":
     case "-a":
       opts['app'] = argv[1];
-      argv.splice(0,1);
+      argv.shift();
       break;
     default:
+      // once we reach an argument that doesn't start with '--' or '-' we stop,
+      // and that argument is the task name
       stop = true;
   }
   if( stop ) {
     break;
   }
-  argv.splice(0,1);
+  argv.shift();
 }
 
-// the global scope that all apps (and objects, I guess) will have access too.
-var project = {config: {}};
+/******** create base app ****/
+app.create(opts.app, function(baseApp) {
+    /******** run requested task *****/
+    if( argv.length == 0) {
+      // they passed in no task, show help message
+      sys.puts('Script for managing Bomber apps.\nhttp://bomber.obtdev.com/');
+      sys.puts('\nAvailable tasks:');
 
-// create the base app for the project
-project.base_app = new App(opts.app, project);
+      Object.keys(tasks).forEach(function(task) {
+          sys.print("  ");
+          sys.puts(task);
+        });
+    }
+    else {
+      if( !(argv[0] in tasks) ) {
+        sys.puts("Unknown task: "+argv[0]);
+      }
+      else {
+        var task = argv.shift();
+        require(tasks[task]).task(baseApp, argv);
+      }
+    }
+  });
 
-//TODO: allow apps to be able to supply their own tasks and load them
-// here
-
-if( opts.tasks ) {
-  sys.puts('Available tasks:');
-  Object.keys(tasks).forEach(function(task) {
-    sys.print("  ");
-    sys.puts(task);
-    });
-}
-else if( argv.length == 0) {
-  sys.puts("Script for managing Bomber apps.\nhttp://bomber.obtdev.com/");
-}
-else {
-  if( !(argv[0] in tasks) ) {
-    sys.puts("Unknown task: "+argv[0]);
-  }
-  else {
-    var task = argv.shift();
-    require(tasks[task]).task(project, argv);
-  }
-}
--- website/assets/ie_html5_fix.js
+++
@@ -1,5 +0,0 @@
-var html5elements = ['article','section','video'];
-for( var i = 0; i < html5elements.length; i++ )
-{
-	document.createElement(html5elements[i]);
-}
